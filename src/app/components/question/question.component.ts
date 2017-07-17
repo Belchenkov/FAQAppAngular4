e483f641fc27f39414d6b0430ee41f46eb72333f
Belchenkov <u608110@gmail.com>
2017-07-17 09:44:41 +0300

    Using Local Storage In The Service

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from "../../services/data.service";
 
 import { Question } from "../../models/question";
 
@@ -10,9 +11,12 @@ import { Question } from "../../models/question";
 export class QuestionComponent implements OnInit {
   @Input('question') question: Question;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
 
+  removeQuestion(question: Question) {
+    this.dataService.removeQuestion(question);
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,7 +7,7 @@ export class DataService {
   questions: Question[];
 
   constructor() { 
-    
+    /*
     this.questions = [
       {
         text: 'What is your name?',
@@ -25,13 +25,49 @@ export class DataService {
         hide: true
       }
     ];
+    */
   }
 
+  // Get Questions from LocalStorage
   getQuestions() {
-      return this.questions;
+    if (localStorage.getItem('questions') === null) {
+      this.questions = [];
+    } else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
+
+    return this.questions;
   }
 
+  // Add Question to LS
   addQuestion(question: Question) {
     this.questions.unshift(question);
+
+    // Init local var
+    let questions;
+
+    if (localStorage.getItem('questions') === null) {
+        questions = [];
+        // Push new Question
+        questions.unshift(question);
+        // Set new array to LS
+        localStorage.setItem('questions', JSON.stringify(questions));
+    } else {
+        questions = JSON.parse(localStorage.getItem('questions'));
+        // Add New Question
+        questions.unshift(question);
+        // Re set LS
+        localStorage.setItem('questions', JSON.stringify(questions));
+    }
+  }
+
+  // Remove Question from LS
+  removeQuestion(question: Question) {
+     for (let i = 0; i < this.questions.length; i++) {
+        if (question == this.questions[i]) {
+          this.questions.splice(i, 1);
+          localStorage.setItem('questions', JSON.stringify(this.questions));
+        }
+     }
   }
 }
